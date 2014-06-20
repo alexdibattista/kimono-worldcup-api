fc7e72a4d8a0693095cd0c18ffba1b0e1b7f17bc
Alexander Di Battista <adibattista@nascentdigital.com>
2014-06-19 23:15:29 -0400

    Fixed index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var kworldcupapi = require('./lib/kimono-worldcup-api.js');
 
 
-var kimonoWorldCup = kworldcupapi('41e39ec62f84f97bcd923b97ca4b359f');
+var kimonoWorldCup = kworldcupapi('');
 
 var players = {};
 
